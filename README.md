1068ee41a2d9392779bda7e9ddf8b036c452e9ad
Jack O'Connor <oconnor663@gmail.com>
2018-11-07 11:30:54 -0500

    use criterion benchmarks

--- README.md
+++ README.md
@@ -2,8 +2,11 @@ This repo currently contains two experiments, both inspired by
 https://twitter.com/zooko/status/1058628340918050816
 
 1. What if Bao used BLAKE2s (`update8`) instead of BLAKE2b (`update4`)?
-2. What if Bao used a 4-way tree instead of a binary tree, to cut down
-   on parent node overhead?
+   This could reduce parent node overhead, because the BLAKE2s block
+   size is exactly two parent nodes.
+2. What if Bao used a 4-way tree instead of a binary tree? This could
+   reduce parent node overhead, because the BLAKE2b block size is
+   exactly four parent nodes.
 
 For now, building this repo requires you to clone these two adjacent to
 it:
@@ -11,8 +14,6 @@ it:
 - https://github.com/oconnor663/blake2b_simd
 - https://github.com/oconnor663/blake2s_simd
 
-Run the benchmarks with:
-
-```
-cargo +nightly bench
-```
+Run the benchmarks with `cargo +nightly bench`. This executes the same
+benchmarks with `libtest` and with
+[`criterion`](https://github.com/japaric/criterion.rs).
---
+++ benches/criterion.rs
@@ -0,0 +1,52 @@
+extern crate bao_experiments;
+extern crate criterion;
+
+use bao_experiments::Finalization::*;
+use criterion::*;
+use std::time::Duration;
+
+// The current 4ary implementation is only defined for inputs that are a power
+// of 4 times the chunk size. 2^24 bytes is about 17 MB.
+const LENGTH: usize = 1 << 24;
+// A long warmup seems important for getting consistent numbers. Intel "Turbo Boost" makes a big
+// difference, but after a few seconds the processor heats up and turns it off.
+const WARMUP_SECS: u64 = 10;
+
+fn bench_standard(c: &mut Criterion) {
+    c.bench(
+        "throughput_benches",
+        Benchmark::new("bench_standard", |b| {
+            let input = vec![0xff; LENGTH];
+            b.iter(move || bao_experiments::hash_recurse_rayon_blake2b(&input, Root(LENGTH as u64)))
+        }).throughput(Throughput::Bytes(LENGTH as u32)),
+    );
+}
+
+fn bench_blake2s(c: &mut Criterion) {
+    c.bench(
+        "throughput_benches",
+        Benchmark::new("bench_blake2s", |b| {
+            let input = vec![0xff; LENGTH];
+            b.iter(move || bao_experiments::hash_recurse_rayon_blake2s(&input, Root(LENGTH as u64)))
+        }).throughput(Throughput::Bytes(LENGTH as u32)),
+    );
+}
+
+fn bench_blake2b_4ary(c: &mut Criterion) {
+    c.bench(
+        "throughput_benches",
+        Benchmark::new("bench_blake2b_4ary", |b| {
+            let input = vec![0xff; LENGTH];
+            b.iter(move || {
+                bao_experiments::hash_recurse_rayon_blake2b_4ary(&input, Root(LENGTH as u64))
+            })
+        }).throughput(Throughput::Bytes(LENGTH as u32)),
+    );
+}
+
+criterion_group!(
+    name = benches;
+    config = Criterion::default().warm_up_time(Duration::from_secs(WARMUP_SECS));
+    targets = bench_standard, bench_blake2s, bench_blake2b_4ary
+);
+criterion_main!(benches);
