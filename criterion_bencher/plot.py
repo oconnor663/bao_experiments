44897afe7de14946f8bd70eb870d67b8d3358f61
Jack O'Connor <oconnor663@gmail.com>
2019-08-18 15:06:19 -0400

    switch plot.py to grouped bar charts
    If I uncomment the "out.svg" line, I find I can get a good PNG thumbnail
    for GitHub with this command:
    
        inkscape -z -e out.png -w 432 -h 216 out.svg
    
    That's just narrow enough for two images on a single row (in the .md
    file view page, which is slightly narrower than the project root), as
    long as there's no whitespace in between them.

--- criterion_bencher/plot.py
+++ criterion_bencher/plot.py
@@ -7,44 +7,43 @@ import pandas
 import seaborn
 import sys
 
-HASH_NAMES = {
-    "bao": "Bao",
-    "blake2b": "BLAKE2b",
-    "blake2bp": "BLAKE2bp",
-    "blake2s": "BLAKE2s",
-    "blake2sp": "BLAKE2sp",
-    "sha256": "OpenSSL SHA-256",
-    "sha512": "OpenSSL SHA-512",
-}
+HASH_NAMES = [
+    ("bao", "Bao"),
+    ("blake2s", "BLAKE2s"),
+    ("blake2b", "BLAKE2b"),
+    ("blake2sp", "BLAKE2sp"),
+    ("blake2bp", "BLAKE2bp"),
+    ("sha256", "OpenSSL SHA-256"),
+    ("sha512", "OpenSSL SHA-512"),
+]
 
-SIZES = {
-    2**6: "64",
-    2**7: "128",
-    2**8: "256",
-    2**9: "512",
-    2**10: "1 KiB",
-    2**11: "2 KiB",
-    2**12: "4 KiB",
-    2**13: "8 KiB",
-    2**14: "16 KiB",
-    2**15: "32 KiB",
-    2**16: "64 KiB",
-    2**17: "128 KiB",
-    2**18: "256 KiB",
-    2**19: "512 KiB",
-    2**20: "1 MiB",
-}
+SIZES = [
+    (2**6, "64"),
+    (2**7, "128"),
+    (2**8, "256"),
+    (2**9, "512"),
+    (2**10, "1 KiB"),
+    (2**11, "2 KiB"),
+    (2**12, "4 KiB"),
+    (2**13, "8 KiB"),
+    (2**14, "16 KiB"),
+    (2**15, "32 KiB"),
+    (2**16, "64 KiB"),
+    (2**17, "128 KiB"),
+    (2**18, "256 KiB"),
+    (2**19, "512 KiB"),
+    (2**20, "1 MiB"),
+]
 
 
 def main():
     target = Path(sys.argv[1])
-    index = [item[1] for item in sorted(SIZES.items())]
-    column_names = []
-    rows = [[] for _ in index]
-    for hash_name, hash_name_pretty in sorted(HASH_NAMES.items()):
-        column_names.append(hash_name_pretty)
+    title = target.with_suffix(".title").open().read().strip()
+    columns = ["function", "size", "throughput"]
+    data = []
+    for hash_name, hash_name_pretty in HASH_NAMES:
         hash_dir = target / "bench_group" / hash_name
-        for (size, size_pretty), row in zip(sorted(SIZES.items()), rows):
+        for size, size_pretty in SIZES:
             estimates_path = hash_dir / str(size) / "new/estimates.json"
             estimates = json.load(estimates_path.open())
             slope = estimates["Slope"]
@@ -52,18 +51,21 @@ def main():
             # upper = slope["confidence_interval"]["upper_bound"]
             # lower = slope["confidence_interval"]["lower_bound"]
             mbps_throughput = size / point * 1000
-            row.append(mbps_throughput)
-    assert len(rows) == len(index), "wrong number of rows"
-    for row in rows:
-        assert len(row) == len(column_names), "wrong number of columns"
-    dataframe = pandas.DataFrame(rows, index=index, columns=column_names)
+            data.append([hash_name_pretty, size_pretty, mbps_throughput])
+    dataframe = pandas.DataFrame(data, columns=columns)
 
     seaborn.set()
-    plot = seaborn.relplot(data=dataframe)
+    pyplot.figure(figsize=[20, 10])
+    seaborn.set_context("talk")
+    plot = seaborn.barplot(data=dataframe,
+                           x="size",
+                           y="throughput",
+                           hue="function")
     plot.set(xlabel="input bytes",
              ylabel="throughput (MB/s)",
-             title="hash function throughput")
+             title=title)
     # plot.set_xticklabels(rotation=30)
+    # pyplot.savefig("out.svg")
     pyplot.show()
 
 
